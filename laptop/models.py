4466e08f65683644ee27fadd4e8c63eba36fd2f8
naww <2310131210005@mhs.ulm.ac.id>
2025-10-16 16:13:35 +0800

    Tambah kolom deskripsi

--- laptop/models.py
+++ laptop/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 class Laptop(models.Model):
+    CATEGORY_CHOICES = [
+        ('entry', 'Entry Level'),
+        ('mid', 'Mid End'),
+        ('high', 'High End'),
+    ]
+
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=50)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default='entry')
     price_per_day = models.DecimalField(max_digits=8, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='laptops/', null=True, blank=True)  # field gambar
+    image = models.ImageField(upload_to='laptops/', null=True, blank=True)
 
     def price_formatted(self):
         return "{:,.0f}".format(self.price_per_day).replace(",", ".")
